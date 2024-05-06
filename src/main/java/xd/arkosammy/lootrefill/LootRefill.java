153b8c598b9aa760ba24c96711af4a6b7fbac273
ArkoSammy12 <jamesvillarreal12@gmail.com>
2024-05-05 21:47:59 -0500

    Added addLootTableId command, update Fabric loader dependency, and update Gradle and Loom versions

--- src/main/java/xd/arkosammy/lootrefill/LootRefill.java
+++ src/main/java/xd/arkosammy/lootrefill/LootRefill.java
@@ -1,13 +1,28 @@
 package xd.arkosammy.lootrefill;
 
+import com.mojang.brigadier.Command;
+import com.mojang.brigadier.tree.ArgumentCommandNode;
+import com.mojang.brigadier.tree.LiteralCommandNode;
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.fabricmc.fabric.api.event.player.PlayerBlockBreakEvents;
 import net.fabricmc.fabric.api.gamerule.v1.GameRuleFactory;
 import net.fabricmc.fabric.api.gamerule.v1.GameRuleRegistry;
 import net.minecraft.SharedConstants;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.LootableContainerBlockEntity;
+import net.minecraft.command.argument.IdentifierArgumentType;
+import net.minecraft.loot.LootDataType;
+import net.minecraft.server.command.CommandManager;
+import net.minecraft.server.command.LootCommand;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.hit.HitResult;
 import net.minecraft.world.GameRules;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,8 +30,8 @@ import xd.arkosammy.lootrefill.util.ducks.LootableContainerBlockEntityAccessor;
 
 public class LootRefill implements ModInitializer {
 
-	public static final String MODID = "lootrefill";
-    public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
+	public static final String MOD_ID = "lootrefill";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 	public static final GameRules.Key<GameRules.IntRule> SECONDS_UNTIL_REFILL = GameRuleRegistry.register("secondsUntilLootRefill", GameRules.Category.MISC, GameRuleFactory.createIntRule(1_800));
 	public static final GameRules.Key<GameRules.IntRule> MAX_REFILLS = GameRuleRegistry.register("maximumAmountOfLootRefills", GameRules.Category.MISC, GameRuleFactory.createIntRule(-1, -1));
 	public static final GameRules.Key<GameRules.BooleanRule> REFILL_ONLY_WHEN_EMPTY = GameRuleRegistry.register("refillLootOnlyWhenEmpty", GameRules.Category.MISC, GameRuleFactory.createBooleanRule(true));
@@ -40,6 +55,52 @@ public class LootRefill implements ModInitializer {
 				return true;
 			}
 			return ((LootableContainerBlockEntityAccessor) lootableContainerBlockEntity).lootrefill$shouldBeProtected(world);
+		});
+
+		CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
+
+			//Root node
+			LiteralCommandNode<ServerCommandSource> lootRefillNode = CommandManager
+					.literal(MOD_ID)
+					.requires(serverCommandSource -> serverCommandSource.hasPermissionLevel(4))
+					.build();
+
+			// Add loot table id command node
+			LiteralCommandNode<ServerCommandSource> addLootTableIdCommand = CommandManager
+					.literal("add_loot_table_id")
+					.requires(serverCommandSource -> serverCommandSource.hasPermissionLevel(4))
+					.build();
+
+			ArgumentCommandNode<ServerCommandSource, Identifier> addLootTableIdArgumentNode = CommandManager
+					.argument("loot_table_id", IdentifierArgumentType.identifier())
+					.suggests(LootCommand.SUGGESTION_PROVIDER)
+					.executes(context -> {
+						Identifier lootTableId = IdentifierArgumentType.getIdentifier(context, "loot_table_id");
+						ServerPlayerEntity player = context.getSource().getPlayerOrThrow();
+						if(!player.getWorld().getServer().getLootManager().getIds(LootDataType.LOOT_TABLES).contains(lootTableId)){
+							player.sendMessage(Text.literal(String.format("The loot table id %s does not exist!", lootTableId)).formatted(Formatting.RED));
+							return Command.SINGLE_SUCCESS;
+						}
+						HitResult hitResult = player.raycast(10, 1, false);
+						if(!(hitResult instanceof BlockHitResult blockHitResult)) {
+							player.sendMessage(Text.literal("You must be looking at a block to use this command!").formatted(Formatting.RED));
+							return Command.SINGLE_SUCCESS;
+						}
+						BlockEntity blockEntity = player.getWorld().getBlockEntity(blockHitResult.getBlockPos());
+						if(!(blockEntity instanceof LootableContainerBlockEntity lootableContainerBlockEntity)) {
+							player.sendMessage(Text.literal("The block you are looking at is not a loot container!").formatted(Formatting.RED));
+							return Command.SINGLE_SUCCESS;
+						}
+						((LootableContainerBlockEntityAccessor) lootableContainerBlockEntity).lootrefill$setCachedLootTableId(lootTableId);
+						player.sendMessage(Text.literal(String.format("Successfully set the loot table id %s for the loot container block at %s!", lootTableId, blockHitResult.getBlockPos().toShortString())).formatted(Formatting.GREEN));
+						return Command.SINGLE_SUCCESS;
+					})
+					.build();
+
+			dispatcher.getRoot().addChild(lootRefillNode);
+			lootRefillNode.addChild(addLootTableIdCommand);
+			addLootTableIdCommand.addChild(addLootTableIdArgumentNode);
+
 		});
 
 	}
